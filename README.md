ff254fa60c3b3925bb7901da5fc92c9f684912eb
krokhale <krishna.rokhale@gmail.com>
2021-06-24 16:59:45 -0700

    checking in

--- README.md
+++ README.md
@@ -3,7 +3,13 @@
 
 ---
 
-> Design the Portfolio box and make it look like the way it  is described in the project.
+> Have the user type out a stock symbol and click on the search button. Then display the search result on the UI
+
+> User should be able to click on the buy button and that should send an API request out to the create a row in the table (This task Depends on you completing a task in the API list first )
+
+>  Show all the stocks that you own in your portfolio in the table on the right hand side. HINT Make a fetch call on page load or use useEffect.
+
+> ~~Design the Portfolio box and make it look like the way it  is described in the project.~~
 
 
 
--- src/App.js
+++ src/App.js
@@ -19,7 +19,9 @@ function App() {
 
                   <div className={'grid grid-cols-12'}>
                       <div className={'col-span-12 md:col-span-6'}>
-                          <input type="search" className={'border w-full p-1 rounded-r-2xl rounded-l-2xl'}/>
+                          <input type="search" className={'border w-2/3 mr-4 p-1 rounded-r-2xl rounded-l-2xl'}/>
+
+                          <button className={'px-6 bg-blue-600 text-white rounded py-2 mr-3'}>Search</button>
                       </div>
 
                       <div className={'hidden md:col-span-6 md:block'}>
@@ -45,9 +47,63 @@ function App() {
 
               </div>
 
-              <div className={'col-span-12 md:col-span-5 border bg-gray-100 h-96'}>
+              <div className={'col-span-12 md:col-span-5 border h-96'}>
 
-                  <h1 className={'text-4xl'}>Portfolio Box</h1>
+
+                  {/*Alternatively, you can also create a table*/}
+                  {/*<table>*/}
+                  {/*    */}
+                  {/*</table>*/}
+
+
+                  <div className={'grid grid-cols-12 p-10'}>
+
+                      <div className={'col-span-3 border bg-gray-500 p-2'}>
+                          <h1 className={'text-white text-center font-bold uppercase text-sm'}>Stock Symbol</h1>
+                      </div>
+
+                      <div className={'col-span-3 border bg-gray-500 p-2'}>
+                          <h1 className={'text-white text-center font-bold uppercase text-sm'}>Quantity</h1>
+                      </div>
+
+                      <div className={'col-span-3 border bg-gray-500 p-2'}>
+                          <h1 className={'text-white text-center font-bold uppercase text-sm'}>Value</h1>
+                      </div>
+
+                      <div className={'col-span-3 border bg-gray-500 p-2'}>
+
+                      </div>
+
+
+                      <div className={'col-span-12'}>
+
+                          <div className={'grid grid-cols-12'}>
+
+                              <div className={'col-span-3 border p-2'}>
+                                  <h1 className={' text-center font-bold uppercase text-sm'}>AAPL</h1>
+                              </div>
+
+                              <div className={'col-span-3 border p-2'}>
+                                  <h1 className={' text-center font-bold uppercase text-sm'}>100</h1>
+                              </div>
+
+                              <div className={'col-span-3 border p-2'}>
+                                  <h1 className={' text-center font-bold uppercase text-sm'}>2000</h1>
+                              </div>
+
+                              <div className={'col-span-3 border p-2'}>
+                                  <h1 className={' text-center font-bold uppercase text-sm'}></h1>
+                              </div>
+                          </div>
+
+
+                      </div>
+
+                  </div>
+
+
+
+
 
               </div>
 
