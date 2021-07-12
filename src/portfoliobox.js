ce648027f13c2e818a6abeb0e408b59695548e85
Lauren Libretti <lalibretti@yahoo.com>
2021-07-12 17:35:59 -0400

    updated .env

--- src/portfoliobox.js
+++ src/portfoliobox.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const URL = `${REACT_APP_API_URL}/api/v1/portfolio`
+const URL = `${process.env.REACT_APP_API_URL}/api/v1/portfolio`
 //const URL = `http://localhost:3000/api/v1/portfolio`
 
 const PortfolioBox = () => {
