87dd8c3408391eb423f3edf9519c64ea8592882c
Lauren Libretti <lalibretti@yahoo.com>
2021-07-13 00:01:19 -0400

    refactored fetch on porfolio

--- src/portfoliobox.js
+++ src/portfoliobox.js
@@ -7,14 +7,24 @@ const URL = `${process.env.REACT_APP_API_URL}/api/v1/portfolio`
 const PortfolioBox = () => {
     const [portfolio, setPortfolio] = useState([])
 
+    const fetchPortfolio = async () => {
+        console.log('runs once then fetches portfolio')
+        let res = await fetch(`${process.env.REACT_APP_API_URL}/api/v1/portfolio`)
+        let data = await res.json()
+        setPortfolio(data)
+    };
+
+
     useEffect(() => {
-        getData()
+       // getData()
+       fetchPortfolio()
     }, [])
 
-    const getData = async () => {
-        const response = await axios.get(URL)
-        setPortfolio(response.data)
-    }
+    // const getData = async () => {s
+    //     const response = await axios .get(URL)
+    //     setPortfolio(response.data)
+    // }
+
     const removeData = (id) => {
 
         axios.delete(`${URL}/${id}`).then(res => {
@@ -24,27 +34,26 @@ const PortfolioBox = () => {
     }
     const renderHeader = () => {
         let headerElement = ['id', 'Symbol', 'Quantity', 'Price', 'Sell']
-
         return headerElement.map((key, index) => {
             return <th key={index}>{key.toUpperCase()}</th>
         })
     }
-    const renderBody = () => {
-        return portfolio && portfolio.map(({id, symbol, quantity, price }) => {
-            console.log(portfolio);
-            return (
-                <tr key={id}>
-                    <td>{id}</td>
-                    <td>{symbol}</td>
-                    <td>{quantity}</td>
-                    <td>{price}</td>
-                    <td className='operation'>
-                        <button className='button-sm px-4 hover:bg-red-300 text-white rounded py-2 mr-1' onClick={() => removeData(id)}>Sell</button>
-                    </td>
-                </tr>
-            )
-        })
-    }
+    // const renderBody = () => {
+    //     return portfolio && portfolio.map()(({id, symbol, quantity, price }) => {
+    //         console.log(portfolio);
+    //         return (
+    //             <tr key={id}>
+    //                 <td>{id}</td>
+    //                 <td>{symbol}</td>
+    //                 <td>{quantity}</td>
+    //                 <td>{price}</td>
+    //                 <td className='operation'>
+    //                     <button className='button-sm px-4 hover:bg-red-300 text-white rounded py-2 mr-1' onClick={() => removeData(id)}>Sell</button>
+    //                 </td>
+    //             </tr>
+    //         )
+    //     })
+    // }
 
     return (
         <>
@@ -58,14 +67,24 @@ const PortfolioBox = () => {
                             <tr>{renderHeader()}</tr>
                         </thead>
                         <tbody className={' text-center font-bold p-2 uppercase text-sm '}>
-                            {renderBody()}
-                        </tbody>
+                                {portfolio && portfolio.map((portfolio) => {
+                                    return <tr>
+                                        <th scope="row">{portfolio.id}</th>
+                                        <td>{portfolio.symbol}</td>
+                                        <td>{portfolio.quantity}</td>
+                                        <td>{portfolio.price}</td>
+                                        <td><button className={'button-sm px-4  hover:bg-red-300 text-white rounded py-2 mr-1'} onClick={() => removeData(portfolio.id)}>Sell</button>
+                                        </td>
+                                    </tr>
+                                })}
+                                {/* {renderBody()} */}
+                            </tbody>
                     </table>
                 </div>
-            </div>
+                </div>
 
         </>
-    )
+            )
 }
 
 
